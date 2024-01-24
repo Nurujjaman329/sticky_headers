9d30756c7081d42d8ec9d27289c6b6fc652be48b
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-01-24 11:46:03 +0600

    sliver add

---
+++ fixed_scroll/lib/homepage.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ScrollController _scrollController = ScrollController();
+  double _appBarOpacity = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      double newOpacity = 7.0 - (_scrollController.offset - 230) / 100;
+      newOpacity = newOpacity.clamp(0.5, 1.0);
+
+      if (newOpacity != _appBarOpacity) {
+        setState(() {
+          _appBarOpacity = newOpacity;
+        });
+      }
+    });
+  }
+
+  SliverGridDelegate _gridDelegate = SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 2,
+    crossAxisSpacing: 8.0,
+    mainAxisSpacing: 8.0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            CustomScrollView(
+              controller: _scrollController,
+              slivers: <Widget>[
+                SliverAppBar(
+                  expandedHeight: 300,
+                  floating: false,
+                  pinned: true,
+                  backgroundColor: Colors.green.withOpacity(_appBarOpacity),
+                  flexibleSpace: LayoutBuilder(
+                    builder: (context, constraints) {
+                      bool isScrollingDown = _scrollController.hasClients &&
+                          _scrollController.position.userScrollDirection ==
+                              ScrollDirection.reverse;
+
+                      double cardTopPosition = isScrollingDown
+                          ? -100
+                          : _scrollController.offset > 300
+                              ? -100
+                              : 60;
+
+                      return Container(
+                        height: 340,
+                        width: double.infinity,
+                        child: Stack(
+                          children: [],
+                        ),
+                        decoration: BoxDecoration(
+                          color: Color(0XFF20A248),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                SliverStickyHeader(
+                  sticky: true,
+                  header: Opacity(
+                    opacity: _appBarOpacity,
+                    child: Container(
+                      height: 60.0,
+                      color: Colors.grey,
+                      child: Center(
+                        child: Text("Header 1"),
+                      ),
+                    ),
+                  ),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, i) => ListTile(
+                        leading: CircleAvatar(
+                          child: Text('0'),
+                        ),
+                        title: Text('List tile #$i'),
+                      ),
+                      childCount: 10,
+                    ),
+                  ),
+                ),
+                SliverGrid(
+                  gridDelegate: _gridDelegate,
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return Container(color: Colors.amber, child: Text('OK'));
+                    },
+                    semanticIndexCallback: (widget, localIndex) {},
+                    childCount: 2,
+                    semanticIndexOffset: 4,
+                  ),
+                ),
+                SliverStickyHeader(
+                  sticky: true,
+                  header: Container(
+                    height: 20.0,
+                    color: Colors.green,
+                  ),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, i) => ListTile(
+                        leading: CircleAvatar(
+                          child: Text('0'),
+                        ),
+                        title: Text('List tile #$i +1'),
+                      ),
+                      childCount: 40,
+                    ),
+                  ),
+                ),
+                SliverStickyHeader(
+                  header: Container(
+                    height: 60.0,
+                    color: Colors.green,
+                  ),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, i) => Container(
+                        child: Image.network(
+                          'https://picsum.photos/400/400',
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: 200.0,
+                        ),
+                      ),
+                      childCount: 3,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
